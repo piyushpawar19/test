05f28330e36e0ac65bffcce92b4820f32f8d3b12
chetan <cmahajan0610@gmail.com>
2025-03-05 11:23:43 +0530

    Number

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/conatact',(req, res) => {
     res.send("welcome to conatct page");
 }); 
 
+app.get('/number',(req, res) => {
+    res.send("welcome to page");
+}); 
+
 const port = 5000;
 
 app.listen(port, ()=>{
